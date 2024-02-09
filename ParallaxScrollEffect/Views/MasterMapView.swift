6584b232b4d09324ee69202704155f659eb58fac
Marcel Zimmermann <mzmann252@gmail.com>
2024-02-09 23:11:17 +0100

    MapView bearbeitet das er interested Places anzeigen kann

---
+++ ParallaxScrollEffect/Views/MasterMapView.swift
@@ -0,0 +1,18 @@
+//
+//  MasterMapView.swift
+//  ParallaxScrollEffect
+//
+//  Created by Marcel Zimmermann on 09.02.24.
+//
+
+import SwiftUI
+
+struct MasterMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MasterMapView()
+}
